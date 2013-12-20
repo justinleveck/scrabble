348a858ca3db77b5c77b9b7fec9d0eeb010bcf91
Justin Leveck <jleveck@gmail.com>
2013-12-19 21:53:13 -0800

    Implement solution

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,5 +1,56 @@
 class Scrabble
+  def self.letter_value
+    {
+      'A'=>1, 'B'=>3, 'C'=>3, 'D'=>2,
+      'E'=>1, 'F'=>4, 'G'=>2, 'H'=>4,
+      'I'=>1, 'J'=>8, 'K'=>5, 'L'=>1,
+      'M'=>3, 'N'=>1, 'O'=>1, 'P'=>3,
+      'Q'=>10, 'R'=>1, 'S'=>1, 'T'=>1,
+      'U'=>1, 'V'=>4, 'W'=>4, 'X'=>8,
+      'Y'=>4, 'Z'=>10
+    }  
+  end
+ 
   def self.score(word)
-    0
+    score = 0
+    word.chars.each do |letter|
+      score += letter_value["#{letter.upcase}"].to_i
+      score
+    end
+    score
+  end
+
+  def self.highest_score_from(words)
+    scores = word_scores(words)
+    highest_scores = scores.select { |k, v| v == scores.values.max }
+    seven_letter_words = seven_letter_words_from(highest_scores.keys)  
+
+    if seven_letter_words.length > 0
+      seven_letter_words.first 
+    else
+      fewest_letters_from(highest_scores.keys)
+    end
+  end
+
+  private
+
+  def self.fewest_letters_from(words)
+    words.sort_by { |word| word.length }.first
+  end
+
+  def self.word_scores(words)
+    scores = {}
+    words.each do |word|
+      scores[word] = score(word)
+    end
+    scores
+  end
+
+  def self.seven_letter_words_from(words)
+    seven_letter_words = []
+    words.each do |word|
+      seven_letter_words << word if word.length == 7
+    end
+    seven_letter_words
   end
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -1,11 +1,76 @@
-Bundler.require
 require './lib/scrabble'
 
 describe Scrabble do
-  describe ".score" do
-    it "scores a single letter" do
-      expect( Scrabble.score("a") ).to eq 1
-      expect( Scrabble.score("f") ).to eq 4
+
+  describe '.letter_value' do
+    it 'returns letter value' do
+      expect( Scrabble.letter_value['A'] ).to eq 1
+    end
+  end
+
+  describe '.score' do
+    it 'scores a single letter' do
+      expect( Scrabble.score('a') ).to eq 1
+      expect( Scrabble.score('f') ).to eq 4
+    end
+
+    it 'scores a word' do
+      expect( Scrabble.score('hello') ).to eq 8
+    end
+  end
+
+  describe '.highest_score_from' do
+    context 'when there IS a point tie' do
+      it 'returns highest scoring word' do
+        expect( Scrabble.highest_score_from(['home', 'word', 'hello', 'sound']) ).to eq 'home'
+      end
+    end
+
+    context 'when there is NOT a POINT TIE' do
+      it 'returns word with the fewest letters' do
+        expect( Scrabble.highest_score_from(['hello', 'word', 'sound']) ).to eq 'word'
+      end
+    end
+
+    context 'when seven letter words' do
+      
+      context 'when ONE of the highest scores is seven letters' do
+        it 'returns seven letter word' do
+          expect( Scrabble.highest_score_from(['hello', 'inker', 'silence']) ).to eq 'silence'
+        end
+      end
+
+      context 'when TWO of the highest scores is seven letters' do
+        it 'returns first seven letter word in array' do
+          expect( Scrabble.highest_score_from(['hello', 'silence', 'license']) ).to eq 'silence'
+        end
+      end
+    end
+
+    context 'when NO seven letter words' do
+      context 'when there IS a point tie AND length tie' do
+        it 'returns the first word in array' do
+          expect( Scrabble.highest_score_from(['shoe', 'hose', 'sound']) ).to eq 'shoe'
+        end
+      end
+    end
+  end
+
+  describe '.fewest_letters_from' do
+    it 'returns a hash of words and score with the fewest lettes' do
+      expect( Scrabble.fewest_letters_from(['hello', 'word']) ).to eq 'word'
+    end
+  end
+
+  describe '.word_scores' do
+    it 'returns a hash with word and score' do
+      expect( Scrabble.send(:word_scores, ['hello', 'word', 'sound']) ).to eq({'hello'=>8, 'word'=>8, 'sound'=>6})
+    end
+  end
+
+  describe '.seven_letter_words_from' do
+    it 'returns a hash of words and scores with seven letters' do
+      expect( Scrabble.seven_letter_words_from(['home', 'word', 'silence']) ).to eq(['silence'])
     end
   end
 end
